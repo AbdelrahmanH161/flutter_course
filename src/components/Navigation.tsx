6e83d435b6866a7c583c81b6cf0aa84c3a50a165
AbdelrahmanH161 <geo.abdelrahmanem@gmail.com>
2025-09-01 12:36:40 +0300

    Add Day 5 link to navigation; enhance user access to new content

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -68,6 +68,11 @@ const Navigation = ({ isDarkMode, toggleTheme }: NavigationProps) => {
 										className='block px-4 py-2 text-sm text-gray-600 dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 hover:text-[#01B5F2] transition-colors duration-200'>
 										Days 4
 									</a>
+									<a
+										href='/day5'
+										className='block px-4 py-2 text-sm text-gray-600 dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 hover:text-[#01B5F2] transition-colors duration-200'>
+										Days 5
+									</a>
 									{/* <a
 										href='/day4'
 										className='block px-4 py-2 text-sm text-gray-600 dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700 hover:text-[#01B5F2] transition-colors duration-200'>
